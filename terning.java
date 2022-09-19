c6c50495421c3550b69be36a633f62cf74fdc6de
Uhrwaldus <CasperUKristensen@gmail.com>
2022-09-19 13:11:30 +0200

    Nyt program

---
+++ terning.java
@@ -0,0 +1,22 @@
+public class terning {
+    public static void main(String[] args) {
+
+        int eyes = 2;
+
+        switch(eyes){
+            case 1 : System.out.print("det er 1");
+            break;
+            case 2 : System.out.print("det er 2");
+            break;
+            case 3 : System.out.print("det er 3");
+            break;
+            case 4 : System.out.print("det er 4");
+            break;
+            case 5 : System.out.print("det er 5");
+            break;
+            case 6 : System.out.print("det er 6");
+            break;
+            default: System.out.print("NEJ!");
+        }
+    }
+}
